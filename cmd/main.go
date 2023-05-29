74f95c5e617b505328604b513fb494b476fe1353
Evgeniy Kochetkov <Evgeniy.Kochetkov@bit-sys.ru>
2023-05-29 23:36:25 +0300

    dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 
 	"github.com/akaniks/todo-app"
 	"github.com/akaniks/todo-app/pkg/handler"
+	"github.com/akaniks/todo-app/pkg/repository"
+	"github.com/akaniks/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
